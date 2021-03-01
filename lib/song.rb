2f999309ec76ace501e6ca9e1e8876135ec3b08a
Sam D <samidiaz3@gmail.com>
2021-03-01 23:08:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,13 +3,19 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@genres - []
 
   def initialize (name, artist, genre)
     @@count += 1
+    @@genres << genre
   end
 
   def self.count
     @@count
+  end
+
+  def self.genre     #array of all genres of exisiting songs
+    @@genres
   end
 
 
